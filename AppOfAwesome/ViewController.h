37d7fdb2a0b257631a43ecbeea5e41d80c58cd3a
megknoll <megknoll@gmail.com>
2014-06-30 18:59:23 -0400

    Initial Commit

---
+++ AppOfAwesome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppOfAwesome
+//
+//  Created by Meghan Knoll Backup on 6/30/14.
+//  Copyright (c) 2014 Meghan Knoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppOfAwesomeTests/AppOfAwesomeTests.m
@@ -0,0 +1,34 @@
+//
+//  AppOfAwesomeTests.m
+//  AppOfAwesomeTests
+//
+//  Created by Meghan Knoll Backup on 6/30/14.
+//  Copyright (c) 2014 Meghan Knoll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppOfAwesomeTests : XCTestCase
+
+@end
+
+@implementation AppOfAwesomeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
